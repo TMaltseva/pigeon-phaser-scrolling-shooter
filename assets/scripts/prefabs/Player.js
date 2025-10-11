a92f16e1c8792990ab0de8711e3300c52ec11156
TMaltseva <bacardeonie@gmail.com>
2025-10-11 08:02:39 -0700

    feat: add player prefab

---
+++ assets/scripts/prefabs/Player.js
@@ -0,0 +1,29 @@
+class Player extends Phaser.GameObjects.Sprite {
+  constructor(scene) {
+    super(scene, 150, config.height / 2, "bird", "bird_0");
+    this.init();
+  }
+
+  init() {
+    this.scene.add.existing(this);
+    this.scene.physics.add.existing(this);
+    this.body.enable = true;
+    this.velocity = 500;
+  }
+
+  move() {
+    this.body.setVelocity(0);
+
+    if (this.scene.cursors.left.isDown) {
+      this.body.setVelocityX(-this.velocity);
+    } else if (this.scene.cursors.right.isDown) {
+      this.body.setVelocityX(this.velocity);
+    }
+
+    if (this.scene.cursors.up.isDown) {
+      this.body.setVelocityY(-this.velocity);
+    } else if (this.scene.cursors.down.isDown) {
+      this.body.setVelocityY(this.velocity);
+    }
+  }
+}
