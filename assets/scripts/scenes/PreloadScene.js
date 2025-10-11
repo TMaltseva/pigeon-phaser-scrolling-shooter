2aae2e7399604b31ac2bfd008a8d3844baf2b3e0
TMaltseva <bacardeonie@gmail.com>
2025-10-11 07:55:45 -0700

    feat: add preload scene with sprites

--- assets/scripts/scenes/PreloadScene.js
+++ assets/scripts/scenes/PreloadScene.js
@@ -2,9 +2,15 @@ class PreloadScene extends Phaser.Scene {
   constructor() {
     super("Preload");
   }
+
   preload() {
-    console.log("Preload assets");
+    this.load.atlas(
+      "bird",
+      "assets/sprites/bird.png",
+      "assets/sprites/bird.json"
+    );
   }
+
   create() {
     this.scene.start("Start");
   }
