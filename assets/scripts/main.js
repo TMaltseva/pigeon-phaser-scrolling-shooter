39442c8d615ddf8ac342c7e4b4667220b5ad6484
TMaltseva <bacardeonie@gmail.com>
2025-10-11 07:53:17 -0700

    feat: add base configs

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,13 @@ let config = {
     type: Phaser.AUTO,
     width: 1280,
     height: 720,
-    scene: [BootScene, PreloadScene, StartScene]
+    scene: [BootScene, PreloadScene, StartScene, GameScene],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    }
 };
 
 let game = new Phaser.Game(config);
