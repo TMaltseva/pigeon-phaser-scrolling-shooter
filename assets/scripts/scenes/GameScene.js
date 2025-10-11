6732c6a2c87e1529830efe9d07b08243e9065314
TMaltseva <bacardeonie@gmail.com>
2025-10-11 07:58:06 -0700

    feat: add Game scene

---
+++ assets/scripts/scenes/GameScene.js
@@ -0,0 +1,22 @@
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("Game");
+  }
+
+  init() {
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  create() {
+    this.createBackground();
+    this.player = new Player(this);
+  }
+
+  update() {
+    this.player.move();
+  }
+
+  createBackground() {
+    this.add.sprite(0, 0, "bg").setOrigin(0);
+  }
+}
