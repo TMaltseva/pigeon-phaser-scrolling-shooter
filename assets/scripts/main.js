f26c9c6df8d7b87570e95c010329b530b4f519da
TMaltseva <bacardeonie@gmail.com>
2025-10-22 15:53:56 -0700

    feat: add plugin for mobile joystick control

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,38 @@
 let config = {
-    type: Phaser.AUTO,
-    width: 1280,
-    height: 720,
-    scene: [BootScene, PreloadScene, StartScene, GameScene],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false
-        }
-    }
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  scene: [BootScene, PreloadScene, StartScene, GameScene],
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false,
+    },
+  },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 400,
+      height: 300,
+    },
+    max: {
+      width: 1920,
+      height: 1080,
+    },
+  },
+  input: {
+    touch: true,
+  },
+  plugins: {
+    global: [
+      {
+        key: "rexVirtualJoystick",
+        plugin: rexvirtualjoystickplugin,
+        start: true,
+      },
+    ],
+  },
 };
 
 let game = new Phaser.Game(config);
