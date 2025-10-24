b322411e0b7ac0c3503068636c9fa5683321cdd7
TMaltseva <bacardeonie@gmail.com>
2025-10-24 06:04:58 -0700

    fix: update gh-pages from main

--- assets/scripts/scenes/PreloadScene.js
+++ assets/scripts/scenes/PreloadScene.js
@@ -4,10 +4,34 @@ class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.add.sprite(0, 0, "bg").setOrigin(0);
-    this.add.sprite(0, 0, "bgp").setOrigin(0);
+    this.isMobile =
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      );
+
+    this.createBackground();
+
     const loadingBar = new LoadingBar(this);
     this.preloadAssets();
+  }
+
+  createBackground() {
+    if (this.isMobile) {
+      this.bg = this.add.image(0, 0, "bgp").setOrigin(0);
+
+      const scaleX = this.scale.width / this.bg.width;
+      const scaleY = this.scale.height / this.bg.height;
+      const scale = Math.max(scaleX, scaleY);
+
+      this.bg.setScale(scale);
+
+      this.bg.setPosition(
+        (this.scale.width - this.bg.displayWidth) / 2,
+        (this.scale.height - this.bg.displayHeight) / 2
+      );
+    } else {
+      this.add.sprite(0, 0, "bg").setOrigin(0);
+    }
   }
 
   preloadAssets() {
